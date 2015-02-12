64e8344c52db7dbcf3a5bda5b58b312fd3fca9a5
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-02-12 15:29:29 -0600

    with ajax, not working

--- skycastapp.js
+++ skycastapp.js
@@ -9,30 +9,35 @@ var app = angular.module('myApp',['google-chart']);
 app.controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
     $scope.address = "Madrid";
     $scope.data = {}
-    // $scope.data.dataTable = {}
+    $scope.data.dataTable = new google.visualization.DataTable();
+
     console.log('Controller Scope: ',$scope.data.dataTable);
 
-    // $scope.getLocation = function(){
-    //     var geocoder = new google.maps.Geocoder();
-    //     geocoder.geocode( {'address': $scope.address }, function(result, status) {
-    //         var lat = result[0].geometry.location.lat();
-    //         var lng = result[0].geometry.location.lng();
-    //         var queryString = 'https://api.forecast.io/forecast/ad95313ec184d7114f912029b416fb5b/' + lat + ',' + lng + "?callback=JSON_CALLBACK";
-    //         getWeatherReport(queryString);
-    //     })
+    $scope.getLocation = function(){
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode( {'address': $scope.address }, function(result, status) {
+            console.log("GEOCODER!")
+            var lat = result[0].geometry.location.lat();
+            var lng = result[0].geometry.location.lng();
+            var queryString = 'https://api.forecast.io/forecast/ad95313ec184d7114f912029b416fb5b/' + lat + ',' + lng + "?callback=JSON_CALLBACK";
+            getWeatherReport(queryString);
+        })
     //     populateTable("hi");
-    // }
+    }
 
-    // function getWeatherReport(query){
-    //     $http.jsonp(query).
-    //         success(populateTable ). // success
-    //         error(function(data) {
-    //             alert('error')
-    //     })
-    // }
+    function getWeatherReport(query){
+        $http.jsonp(query).
+            success(function(){
+                console.log("Get Weather!")
+                populateTable('value')
+            }). // success
+            error(function(data) {
+                alert('error')
+        })
+    }
 
-    // function populateTable(results) {
-        $scope.data.dataTable = new google.visualization.DataTable();
+    function populateTable(results) {
+        console.log('Populate Table!');
         $scope.data.dataTable.addColumn('string', 'Summary');
         $scope.data.dataTable.addColumn('number', 'Temperature');
         $scope.data.dataTable.addColumn('number', 'Probability of precipitation');
@@ -49,7 +54,7 @@ app.controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
             ['Eggplant', 1, 1],
             ['Pepperoni', 2, 1]
         ]);
-    // }
+    }
 
 }]) // controller
 
@@ -62,7 +67,6 @@ var googleChart = googleChart || angular.module("google-chart",[]);
 googleChart.directive("googleChart",function(){      
     return {
         restrict : "A",
-        // scope : false,
         link: function(scope, elem, attr) {
             console.log('Directive Scope: ', scope.data)
             // scope.$watch('data', function(newValue, oldValue) { 
