e2c1dabfe3f094ef11d02872946ac1ae933fa6e7
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-02-12 15:18:36 -0600

    table working without ajax

--- skycastapp.js
+++ skycastapp.js
@@ -9,36 +9,37 @@ var app = angular.module('myApp',['google-chart']);
 app.controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
     $scope.address = "Madrid";
     $scope.data = {}
+    // $scope.data.dataTable = {}
+    console.log('Controller Scope: ',$scope.data.dataTable);
 
-    $scope.getLocation = function(){
-        // var geocoder = new google.maps.Geocoder();
-        // geocoder.geocode( {'address': $scope.address }, function(result, status) {
-        //     var lat = result[0].geometry.location.lat();
-        //     var lng = result[0].geometry.location.lng();
-        //     var queryString = 'https://api.forecast.io/forecast/ad95313ec184d7114f912029b416fb5b/' + lat + ',' + lng + "?callback=JSON_CALLBACK";
-        //     getWeatherReport(queryString);
-        // })
-        populateTable("hi");
-    }
+    // $scope.getLocation = function(){
+    //     var geocoder = new google.maps.Geocoder();
+    //     geocoder.geocode( {'address': $scope.address }, function(result, status) {
+    //         var lat = result[0].geometry.location.lat();
+    //         var lng = result[0].geometry.location.lng();
+    //         var queryString = 'https://api.forecast.io/forecast/ad95313ec184d7114f912029b416fb5b/' + lat + ',' + lng + "?callback=JSON_CALLBACK";
+    //         getWeatherReport(queryString);
+    //     })
+    //     populateTable("hi");
+    // }
 
-    function getWeatherReport(query){
-        $http.jsonp(query).
-            success(populateTable ). // success
-            error(function(data) {
-                alert('error')
-        })
-    }
+    // function getWeatherReport(query){
+    //     $http.jsonp(query).
+    //         success(populateTable ). // success
+    //         error(function(data) {
+    //             alert('error')
+    //     })
+    // }
 
-    function populateTable(results) {
-        // $scope.data = {};
+    // function populateTable(results) {
         $scope.data.dataTable = new google.visualization.DataTable();
         $scope.data.dataTable.addColumn('string', 'Summary');
         $scope.data.dataTable.addColumn('number', 'Temperature');
         $scope.data.dataTable.addColumn('number', 'Probability of precipitation');
         // data.addRows([
-        //   results.currently.summary,
-        //   results.currently.temperature,
-        //   results.currentlu.precipitProbability
+          // results.currently.summary,
+          // results.currently.temperature,
+          // results.currentlu.precipitProbability
         // ]);
 
         $scope.data.dataTable.addRows([
@@ -48,7 +49,7 @@ app.controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
             ['Eggplant', 1, 1],
             ['Pepperoni', 2, 1]
         ]);
-    }
+    // }
 
 }]) // controller
 
@@ -61,21 +62,23 @@ var googleChart = googleChart || angular.module("google-chart",[]);
 googleChart.directive("googleChart",function(){      
     return {
         restrict : "A",
-        transclude: true,
-        link: function(scope, elem, attrs) {
-            scope.$watch('data', function(newValue, oldValue) { 
-                if (scope.data) {       
-                    scope.data.dataTable = newValue;
+        // scope : false,
+        link: function(scope, elem, attr) {
+            console.log('Directive Scope: ', scope.data)
+            // scope.$watch('data', function(newValue, oldValue) { 
+                // console.log(newValur)
+                // if (scope.data) {       
+                    // scope.data.dataTable = newValue;
 
-                    var dt = scope[elem.ngModel].dataTable;
+                    var dt = scope.data.dataTable;
                     var options = {'title':'Current Weather',
                                     'width':400,
                                     'height':300};
 
                     var googleChart = new google.visualization[attr.googleChart](elem[0]);
                     googleChart.draw(dt,options);
-                }
-            });
+                // }
+            // });
         }
     }
 });
