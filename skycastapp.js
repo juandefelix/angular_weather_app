11fd55d8f7682354f4541851f9b839be774c96a6
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-02-13 15:15:41 -0600

    deleted unused files

--- skycastapp.js
+++ skycastapp.js
@@ -12,8 +12,6 @@ app.controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
     $scope.data.dataTable = new google.visualization.DataTable();
     $scope.isReady = false;
 
-    console.log('Controller Scope: ',$scope.data.dataTable);
-
     $scope.getLocation = function(){
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode( {'address': $scope.address }, function(result, status) {
@@ -23,41 +21,30 @@ app.controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
             var queryString = 'https://api.forecast.io/forecast/ad95313ec184d7114f912029b416fb5b/' + lat + ',' + lng + "?callback=JSON_CALLBACK";
             getWeatherReport(queryString);
         })
-    //     populateTable("hi");
     }
 
     function getWeatherReport(query){
         $http.jsonp(query).
-            success(function(){
-                console.log("Get Weather!")
-                populateTable('value')
+            success(function(data){
+                populateTable(data)
             }). // success
             error(function(data) {
-                alert('error')
+                alert('error');
         })
     }
 
     function populateTable(results) {
-        console.log('Populate Table!');
+        console.log(results);
         $scope.data.dataTable.addColumn('string', 'Summary');
         $scope.data.dataTable.addColumn('number', 'Temperature');
         $scope.data.dataTable.addColumn('number', 'Probability of precipitation');
-        // data.addRows([
-          // results.currently.summary,
-          // results.currently.temperature,
-          // results.currentlu.precipitProbability
-        // ]);
-
         $scope.data.dataTable.addRows([
-            ['Mushrooms', 3, 1],
-            ['Onions', 1, 1],
-            ['Olives', 1, 1],
-            ['Eggplant', 1, 1],
-            ['Pepperoni', 2, 1]
+          results.currently.summary,
+          results.currently.temperature,
+          results.currently.precipProbability
         ]);
 
         $scope.isReady = true;
-        console.log('FINAL: ', $scope.data.dataTable)
     }
 }]) // controller
 
@@ -70,12 +57,9 @@ var googleChart = googleChart || angular.module("google-chart",[]);
 googleChart.directive("googleChart",function(){      
     return {
         restrict : "A",
-        // replace: true,
         link: function(scope, elem, attr) {
-            console.log('Directive Scope: ', scope.data)
             scope.$watch('isReady', function(newValue, oldValue) { 
                 if (newValue) {    
-                console.log("NEW")  
                     var dt = scope.data.dataTable;
                     var options = { 'title':'Current Weather',
                                     'width':400,
@@ -87,31 +71,8 @@ googleChart.directive("googleChart",function(){
                 }
                 else { 
                     $(elem).hide(); 
-                    console.log("OLD")
                 }
             });
         }
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
